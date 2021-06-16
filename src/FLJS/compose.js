02611d09c33711b0048cba6a17ebb5efb70af6f1
henry <cksgod16@gmail.com>
2021-06-16 14:58:45 +0900

    Update

--- src/FLJS/compose.js
+++ src/FLJS/compose.js
@@ -29,12 +29,22 @@ second function call.";
 
 let wordsFound = words(text);
 let wordsUsed = unique(wordsFound);
-let newWordsUsed = unique(words(text)); // read left-to-right --the order of operations right-to-left
-
-console.log(wordsUsed);
+let newWordsUsed = unique(words(text)); // read left-to-right --the order of operations right-to-lefts
 
 // uniqueWrods composition of the two functions
 // wordsUsed <-- unique <-- words <-- text
 function uniqueWords(str) {
   return unique(words(str));
 }
+
+function compose2(fn2, fn1) {
+  return function composed(origValue) {
+    return fn2(fn1(origValue)); //.. higher-order function
+  };
+}
+
+let newUniqueWords = compose2(unique, words);
+let letters = compose2(words, unique);
+// let chars = newUniqueWords("How are you Henry?"); // ['how', 'are', 'you', 'henry']
+// let chars = letters("How are you Henry?"); //[ 'h', 'o', 'w', 'a', 'r', 'e', 'y', 'u', 'n' ]
+console.log(chars);
