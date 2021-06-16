fd379f8f3d26f145c8e51b1a308b740b8ac104b7
henry <cksgod16@gmail.com>
2021-06-16 17:46:54 +0900

    Refactoring utils

--- src/FLJS/composition.js
+++ src/FLJS/composition.js
@@ -1,34 +1,23 @@
 // functional-light-js chapter 4
-
-function words(str) {
-  return String(str)
-    .toLowerCase()
-    .split(/\s|\b/)
-    .filter(function alpha(v) {
-      return /^[\w]+$/.test(v);
-    });
-}
-
-function unique(list) {
-  let uniqList = [];
-
-  for (let v of list) {
-    // value not yet in the new list?
-    if (uniqList.indexOf(v) === -1) {
-      uniqList.push(v);
-    }
-  }
-
-  return uniqList;
-}
+const {
+  partialRight,
+  compose2,
+  unique,
+  words,
+  compose,
+  skipShortWords,
+} = require("./utils/index");
 
 let text =
   "To compose two functions together, pass the \
 output of the first function call as the input of the \
 second function call.";
 
+// 일반적인 사용례
 let wordsFound = words(text);
 let wordsUsed = unique(wordsFound);
+
+// 고차 함수!
 let newWordsUsed = unique(words(text)); // read left-to-right --the order of operations right-to-lefts
 
 // uniqueWrods composition of the two functions
@@ -37,14 +26,21 @@ function uniqueWords(str) {
   return unique(words(str));
 }
 
-function compose2(fn2, fn1) {
-  return function composed(origValue) {
-    return fn2(fn1(origValue)); //.. higher-order function
-  };
-}
-
 let newUniqueWords = compose2(unique, words);
 let letters = compose2(words, unique);
+
 // let chars = newUniqueWords("How are you Henry?"); // ['how', 'are', 'you', 'henry']
-// let chars = letters("How are you Henry?"); //[ 'h', 'o', 'w', 'a', 'r', 'e', 'y', 'u', 'n' ]
-console.log(chars);
+let chars = letters("How are you Henry?"); //[ 'h', 'o', 'w', 'a', 'r', 'e', 'y', 'u', 'n' ]
+// 순서에 따라 값이 변한다. -> 순서가 중요하다 ?
+
+/**
+ * @content { general composition }
+ */
+
+// let biggerWords = compose(skipShortWords, unique, words);
+// let wordsbig = biggerWords(text);
+
+let filterWords = partialRight(compose, unique, words);
+let shorterWords = filterWords(skipShortWords);
+
+console.log(shorterWords(text));
---
+++ src/FLJS/utils/index.js
@@ -0,0 +1,76 @@
+function words(str) {
+  return String(str)
+    .toLowerCase()
+    .split(/\s|\b/)
+    .filter(function alpha(v) {
+      return /^[\w]+$/.test(v);
+    });
+}
+
+function unique(list) {
+  let uniqList = [];
+  for (let v of list) {
+    if (uniqList.indexOf(v) === -1) {
+      uniqList.push(v);
+    }
+  }
+  return uniqList;
+}
+
+function compose2(fn2, fn1) {
+  return function composed(origValue) {
+    return fn2(fn1(origValue)); //.. higher-order function
+  };
+}
+
+function partialRight(fn, ...presetArgs) {
+  return function partiallyApplied(...laterArgs) {
+    return fn(...laterArgs, ...presetArgs);
+  };
+}
+
+function compose(...fns) {
+  return function composed(result) {
+    let list = [...fns];
+
+    while (list.length > 0) {
+      result = list.pop()(result);
+    }
+
+    return result;
+  };
+}
+
+function skipShortWords(words) {
+  var filteredWords = [];
+
+  for (let word of words) {
+    if (word.length <= 4) {
+      filteredWords.push(word);
+    }
+  }
+
+  return filteredWords;
+}
+
+// left-to-right
+function pipe(...fns) {
+  return function piped(result) {
+    let list = [...fns];
+
+    while (list.length > 0) {
+      result = list.shift()(result);
+    }
+    return result;
+  };
+}
+
+module.exports = {
+  partialRight,
+  words,
+  unique,
+  compose2,
+  compose,
+  pipe,
+  skipShortWords,
+};
